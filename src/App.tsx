084b5a693c2f11600f1520228bd42c20493286e1
Ian Gabriel <i.gabrielgeneralao@gmail.com>
2024-05-03 23:15:18 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Landing, Login, Logout, SharedLayout, Dashboard, AddJob, EditUser, Jobs } from './pages';
+
+function App() {
+    const routes = createBrowserRouter([
+        {
+            path: '/',
+            element: <Landing />,
+        },
+        {
+            path: '/app',
+            element: <SharedLayout />,
+            children: [
+                {
+                    index: true,
+                    element: <Dashboard />,
+                },
+                {
+                    path: 'addJob',
+                    element: <AddJob />,
+                },
+                {
+                    path: 'editUser',
+                    element: <EditUser />,
+                },
+                {
+                    path: 'jobs',
+                    element: <Jobs />,
+                },
+            ],
+        },
+        {
+            path: '/login',
+            element: <Login />,
+        },
+        {
+            path: '/logout',
+            element: <Logout />,
+        },
+    ]);
+    return <RouterProvider router={routes} />;
+}
+
+export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export { default as Landing } from './Landing/Landing';
+export { default as Logout } from './Auth/Logout';
+export { default as Login } from './Auth/Login';
+export { default as SharedLayout } from './Layout/SharedLayout';
+export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as AddJob } from './AddJob/AddJob';
+export { default as EditUser } from './EditUser/EditUser';
+export { default as Jobs } from './Jobs/Jobs';
