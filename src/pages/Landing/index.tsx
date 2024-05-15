1105cf671997af3d495e49434e5959639c505e23
Ian Gabriel <i.gabrielgeneralao@gmail.com>
2024-05-14 21:38:15 -0400

    Change landing page layout

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,21 +2,18 @@ import { Link } from 'react-router-dom';
 
 function Landing() {
     return (
-        <main className="h-screen w-screen bg-slate-50 grid place-items-center">
-            <div className="border-2 border-black size-9/12 grid grid-cols-2 gap-5 p-8">
-                <div className="border-2 border-black p-2">
-                    {/* TODO: Think of better message and layout. Check dribble */}
-                    <h1 className="text-7xl tracking-wide text-gray-500">
-                        Unemployed? <span className="text-indigo-500">Not for long</span>
-                    </h1>
-                    <div>
-                        <button className="bg-black text-white border-2 border-black py-1 px-2 rounded-md">
-                            <Link to="/login">Get Started</Link>
-                        </button>
-                    </div>
+        <main className="h-screen w-screen bg-indigo-100 grid place-items-center">
+            <div className="border-2 border-black px-16 size-11/12 flex flex-col justify-center gap-y-12 items-center">
+                {/* TODO: Think of better message and layout. Check dribble */}
+                <h1 className="text-7xl text-center tracking-wide text-gray-500">
+                    Because your <span className="text-indigo-500 uppercase">Job Search</span> saga deserves its own
+                    dashboard
+                </h1>
+                <div>
+                    <button className="bg-black text-white border-2 border-black py-1 px-2 rounded-md">
+                        <Link to="/login">Get Started</Link>
+                    </button>
                 </div>
-                {/* TODO: Add image */}
-                <div className="border-2 border-black">image</div>
             </div>
         </main>
     );
